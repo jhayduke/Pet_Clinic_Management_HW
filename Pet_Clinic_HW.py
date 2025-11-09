9238aa118143abb1dad5bc00f1ff3a3c2903055c
jhayduke <joshua.hayduke@gmail.com>
2025-11-09 10:48:46 -0700

    fixed import sqlalchemy

--- Pet_Clinic_HW.py
+++ Pet_Clinic_HW.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, String, Integer, ForeignKey
+from sqlalchemy import create_engine, import Integer, String,ForeignKey
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker, Mapped, mapped_column
 from datetime import datetime
 
@@ -13,25 +13,25 @@ Base = declarative_base()
 
 class Owner(Base):
     __tablename__ = 'Owners'
-    id: Mapped[int] = mapped_column(int, unique=True, primary_key=True)
-    name: Mapped[String] = mapped_column(str(150), unique=True, nullable=False)
-    phone:  Mapped[int] = mapped_column(str(25), unique=True, nullable=False)
-    email: Mapped[str] = mapped_column(str(150), unique=True)
+    id: Mapped[int] = mapped_column(Integer, unique=True, primary_key=True)
+    name: Mapped[String] = mapped_column(String(150), unique=True, nullable=False)
+    phone:  Mapped[int] = mapped_column(String(25), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(150), unique=True)
 
 class Pet(Base):
     __tablename__ = 'Pets'
     id: Mapped[int] = mapped_column(unique=True, primary_key=True)
     name: Mapped[str] = mapped_column(String(150), unique=True, nullable=False)
     species: Mapped[str] = mapped_column(String(150), nullable=False)
-    breed: Mapped[str] = mapped_column(str(150), nullable=False)
-    owner_id: Mapped[int] = mapped_column(int(150), unique=True, ForeignKey('Owners.id'))
+    breed: Mapped[str] = mapped_column(String(150), nullable=False)
+    owner_id: Mapped[int] = mapped_column(Integer, unique=True, ForeignKey('Owners.id'))
 
 class Vet(Base):
     __tablename__ = 'Vets'
-    id: Mapped[int] = mapped_column(int(150), unique=True, primary_key=True)
-    name: Mapped[str] = mapped_column(str(150), unique=True, nullable=False)
-    specialization: Mapped[str] = mapped_column(str(250), nullable=True)
-    email: Mapped[str] = mapped_column(str(150), unique=True, Nullable=True)
+    id: Mapped[int] = mapped_column(Integer, unique=True, primary_key=True)
+    name: Mapped[str] = mapped_column(String(150), unique=True, nullable=False)
+    specialization: Mapped[str] = mapped_column(String(250), nullable=True)
+    email: Mapped[str] = mapped_column(String(150), unique=True, Nullable=True)
 
 
 
