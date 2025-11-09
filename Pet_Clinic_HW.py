73a228df7f16d1c1577fde6c4cd08f31b299619a
jhayduke <joshua.hayduke@gmail.com>
2025-11-09 10:39:48 -0700

    further refinement

--- Pet_Clinic_HW.py
+++ Pet_Clinic_HW.py
@@ -1,10 +1,10 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, String, Integer, ForeignKey
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker, Mapped, mapped_column
 from datetime import datetime
 
 
-session = Session() 
 Session = sessionmaker(bind=engine)
+session = Session() 
 
 engine = create_engine('sqlite:///pet_clinic_HW.db')
 
@@ -13,9 +13,9 @@ Base = declarative_base()
 
 class Owner(Base):
     __tablename__ = 'Owners'
-    id: Mapped[int] = mapped_column(unique=True, primary_key=True)
+    id: Mapped[int] = mapped_column(int, unique=True, primary_key=True)
     name: Mapped[String] = mapped_column(str(150), unique=True, nullable=False)
-    phone:  Mapped[int] = mapped_column(str(12), unique=True, nullable=False)
+    phone:  Mapped[int] = mapped_column(str(25), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(str(150), unique=True)
 
 class Pet(Base):
@@ -23,8 +23,8 @@ class Pet(Base):
     id: Mapped[int] = mapped_column(unique=True, primary_key=True)
     name: Mapped[str] = mapped_column(String(150), unique=True, nullable=False)
     species: Mapped[str] = mapped_column(String(150), nullable=False)
-    breed: Mapped[str] = mapped_column(str, nullable=False)
-    owner_id: Mapped[int] = mapped_column(int(150), unique=Tre, ForeignKey=True)
+    breed: Mapped[str] = mapped_column(str(150), nullable=False)
+    owner_id: Mapped[int] = mapped_column(int(150), unique=True, ForeignKey('Owners.id'))
 
 class Vet(Base):
     __tablename__ = 'Vets'
@@ -38,4 +38,4 @@ class Vet(Base):
 
 
 
-    Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
